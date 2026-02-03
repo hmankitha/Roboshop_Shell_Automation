754e720ac0a96abd5cdb82fe9ca9a7cac9975282
Adii <adii...@Adiis-MacBook-Air.local>
2026-02-03 12:10:38 +0530

    learning

--- mongodb.sh
+++ mongodb.sh
@@ -3,20 +3,20 @@
 USERID=$(id -u)
 LOGS_FOLDER="/var/log/shell-roboshop"
 LOGS_FILE="$LOGS_FOLDER/$0.log"
-R="\e[31"
-G="\e[32"
-Y="\e[33"
-N="\e[34"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
-if [ $USERID -nq 0 ]; then 
-    echo -e "$R Please run this script with root user access $N | tee -a $LOGS_FILE
+if [ $USERID -ne 0 ]; then 
+    echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
     exit 1
 fi 
 
-mkdir -p $LOGS_FOlDER
+mkdir -p $LOGS_FOLDER
 
 VALIDATE(){
-    if [ $1 -nq 0 ]; then
+    if [ $1 -ne 0 ]; then
         echo -e "$2 ... $R FAILURE $N" | tee -a $LOGS_FILE
     else 
         echo -e "$2 ... $G SUCESS $N" | tee -a $LOGS_FILE
@@ -36,7 +36,7 @@ VALIDATE $? "Enable mongod"
 systemctl start mongod
 VALIDATE $? "started mongod"
 
-sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongo.conf
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
 VALIDATE $? "Allowing remote connections" 
 
 systemctl restart mongod
