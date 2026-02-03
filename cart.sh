97ba1e90bf82cae22e72f17a0f118923da2989af
Adii <adii...@Adiis-MacBook-Air.local>
2026-02-03 19:45:08 +0530

    learning

---
+++ cart.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+SCRIPT_DIR=$PWD
+MONGODB_HOST=mongodb.ankitha.online
+
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOGS_FILE
+    else 
+        echo -e "$2 ... $G SUCESS $N" | tee -a $LOGS_FILE
+    fi    
+
+}
+
+dnf module disable nodejs -y &>>$LOGS_FILE
+VALIDATE $? "Disabling Nodejs Default version"
+
+dnf module enable nodejs:20 -y &>>$LOGS_FILE
+VALIDATE $? "Enabling NodeJS 20"
+
+dnf install nodejs -y &>>$LOGS_FILE
+VALIDATE $? "Install NodeJS"
+
+id roboshop &>>$LOGS_FILE
+if [ $? -ne 0 ]; then
+
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+    VALIDATE $? "Creating system user"
+else 
+    echo -e "Roboshop user already exit ...$Y SKIPPING $N"
+fi
+
+mkdir -p /app &>>$LOGS_FILE
+VALIDATE $? "creating app directory"
+
+curl -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip &>>$LOGS_FILE
+VALIDATE $? "Downloading cart code"
+
+cd /app &>>$LOGS_FILE
+VALIDATE $? "Moving to app directory"
+
+rm -rf /app/*
+VALIDATE $? "Removing existing code"
+
+unzip /tmp/cart.zip &>>$LOGS_FILE
+VALIDATE $? "unziping the file"
+
+npm install &>>$LOGS_FILE
+VALIDATE $? "installing the build tool"
+
+cp $SCRIPT_DIR/cart.service /etc/systemd/system/cart.service &>>$LOGS_FILE
+VALIDATE $? "Created systemctl services"
+
+systemctl daemon-reload &>>$LOGS_FILE
+systemctl enable cart &>>$LOGS_FILE
+systemctl start cart &>>$LOGS_FILE
+VALIDATE $? "Starting and enabling Catalogue"
+
+cp $SCRIPT_DIR/mongo.repo/etc/yum.repos.d/mongo.repo
+dnf install mongodb-mongosh -y &>>$LOGS_FILE
+
+INDEX=$(mongosh --host $MONGODB_HOST --quiet  --eval 'db.getMongo().getDBNames().indexOf("cart")')
+
+if [ $INDEX -le 0 ]; then
+    mongosh --host $MONGODB_HOST </app/db/master-data.js
+    VALIDATE $? "Loading products"
+else
+    echo -e "Products already loaded ... $Y SKIPPING $N"
+fi
+
+systemctl restart cart
+VALIDATE $? "Restarting cart"
+
